d3fbf8b06b31e7fa959e9884d459b71752eac097
Namish Maheshwari <namishmaheshwari@Namishs-MacBook-Pro.local>
2019-01-14 13:32:44 -0800

    first commit

---
+++ WebScrap/models/Stream.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace WebScrapper.models
+{
+    internal class Stream
+    {
+        public string Name
+        {
+            get;
+            set;
+        }
+        public List<Major> Majors{
+            get;
+            set;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Stream Name: {0}\r\n Streams Majors:\r\n {1}", this.Name, string.Join("\n", this.Majors));
+
+        }
+    }
+}
---
+++ WebScrap/models/Student.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+
+namespace WebScrapper.models
+{
+    public class Student
+    {
+        public Student(){
+            this.Major = new List<string>();
+            this.School = new List<string>();
+            this.Degree = new List<string>();
+            this.additionalInfo = new List<string>();
+        }
+        public string Name
+        {
+            get;
+            set;
+        }
+        public List<string> Major
+        {
+            get;
+            set;
+        }
+        public List<string> School
+        {
+            get;
+            set;
+        }
+        public List<string> Degree
+        {
+            get;
+            set;
+        }
+        public List<string> additionalInfo{
+            get;
+            set;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Student Name:{0}\r\nStudent Major:{1}\r\n Student School:{2}\r\n Student Degree:{3}\r\nStudent AddInfo:{4}\r\n", 
+                                 this.Name, string.Join(",", this.Major), string.Join(",", this.School),
+                                 string.Join(",", this.Degree), string.Join(",", this.additionalInfo));
+        }
+
+    }
+}
